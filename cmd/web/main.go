24e8076de43b56be0d01e20269fb2f1ae3446ad1
Pacharapon Phuangtong <pacharaponphuangtong@Pacharapons-MacBook-Air.local>
2025-03-18 12:50:18 +0700

    managing configuration setting using command-line flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
 func main() {
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.staticDir))
 	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("starting server on %v, static-dir: %v", cfg.addr, cfg.staticDir)
+	err := http.ListenAndServe(cfg.addr, mux)
 	log.Fatal(err)
 }
