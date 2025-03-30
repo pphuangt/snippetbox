ad68fd0adb5c05a10cea11c175c9edd95eed1946
Pacharapon Phuangtong <pacharaponphuangtong@Pacharapons-MacBook-Air.local>
2025-03-31 03:58:48 +0700

    database-driven responses

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,28 +1,41 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"html/template"
 	"net/http"
+	"snippedbox/internal/models"
 	"strconv"
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	files := []string{
-		"./ui/html/base.tmpl.html",
-		"./ui/html/partials/nav.tmpl.html",
-		"./ui/html/pages/home.tmpl.html",
-	}
-	ts, err := template.ParseFiles(files...)
+
+	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, r, err)
 		return
 	}
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.serverError(w, r, err)
+
+	for _, snippet := range snippets {
+		fmt.Fprintf(w, "%+v\n", snippet)
 	}
+	/*
+		files := []string{
+			"./ui/html/base.tmpl.html",
+			"./ui/html/partials/nav.tmpl.html",
+			"./ui/html/pages/home.tmpl.html",
+		}
+		ts, err := template.ParseFiles(files...)
+		if err != nil {
+			app.serverError(w, r, err)
+			return
+		}
+		err = ts.ExecuteTemplate(w, "base", nil)
+		if err != nil {
+			app.serverError(w, r, err)
+		}
+	*/
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +45,16 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+	s, err := app.snippets.Get(id)
+	if err != nil {
+		if errors.Is(err, models.ErrNoRecord) {
+			http.NotFound(w, r)
+		} else {
+			app.serverError(w, r, err)
+		}
+		return
+	}
+	fmt.Fprintf(w, "%+v", s)
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -40,6 +62,14 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Save a new snippet..."))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	_ "github.com/go-sql-driver/mysql"
 	"log/slog"
 	"net/http"
 	"os"
+	"snippedbox/internal/models"
 )
 
 type application struct {
-	logger *slog.Logger
-	config config
+	logger   *slog.Logger
+	snippets *models.SnippetModel
+	config   config
 }
 
 type config struct {
 	addr      string
 	staticDir string
+	dsn       string
 }
 
 func main() {
@@ -23,10 +28,33 @@ func main() {
 	app := &application{logger: logger}
 	flag.StringVar(&app.config.addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&app.config.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.StringVar(&app.config.dsn, "dsa", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
+	db, err := openDB(app.config.dsn)
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+	defer db.Close()
+	app.snippets = &models.SnippetModel{DB: db}
+
 	logger.Info("starting server on", slog.Any("addr", app.config.addr))
-	err := http.ListenAndServe(app.config.addr, app.routes())
+	err = http.ListenAndServe(app.config.addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
